b01659010945a6583516b9345543c281ae59effe
kailash <kailashrajut0575@gmail.com>
2023-10-23 20:56:07 +0530

    navbar_done

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ function App() {
     <div className="main_container_navbar bg-slate-950">
 
     <div className="container mx-auto py-4">
-      <nav className="flex justify-between">
+      <nav className="flex justify-between ">
         <div>
           {" "}
           <img src={Logo} alt="navbar_logo" />
         </div>
-        <div className="flex text-lg text-white  list-none gap-5">
-          <li>Home</li>
-          <li>Services</li>
-          <li> Company</li>
-          <li>Careen</li>
-          <li>Blog</li>
-          <li>Contact US</li>
-          <li>Clone project</li>
+        <div className="flex text-[16px] text-white  list-none gap-5">
+          <li>
+            <a href="">Home</a>
+            </li>
+          <li><a href="">Services</a></li>
+          <li> <a href="">Company</a></li>
+          <li><a href="">Careen</a></li>
+          <li>
+            <a href="">Blog</a>
+          </li>
+          <li><a href="">Contact US</a></li>
+          <li><a href="">Clone project</a></li>
 
         </div>
       </nav>
