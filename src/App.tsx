1c9390cd80d06c33f6989c31626be408e4dc4270
kailash <kailashrajut0575@gmail.com>
2023-10-23 22:47:18 +0530

    hero_section_done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Logo from "../src/assets/images/navbar_image/Logo.svg";
+import hero_image from '../src/assets/images/hero_section_image/hero_image.png'
 function App() {
   return (
     <div className="main_container_navbar bg-slate-950">
@@ -12,19 +13,32 @@ function App() {
         </div>
         <div className="flex text-[16px] text-white  list-none gap-5">
           <li>
-            <a href="">Home</a>
+            <a href="/">Home</a>
             </li>
-          <li><a href="">Services</a></li>
-          <li> <a href="">Company</a></li>
-          <li><a href="">Careen</a></li>
+          <li><a href="/">Services</a></li>
+          <li> <a href="/">Company</a></li>
+          <li><a href="/">Careen</a></li>
           <li>
-            <a href="">Blog</a>
+            <a href="/">Blog</a>
           </li>
-          <li><a href="">Contact US</a></li>
-          <li><a href="">Clone project</a></li>
+          <li><a href="/">Contact US</a></li>
+          <li><a href="/">Clone project</a></li>
 
         </div>
       </nav>
+      <header>
+        <div className=" flex justify-between py-12  items-center ">
+          <div className="left_hero_section text-white  ">
+          <h1 className="text-[56px] w-[60%] ">Transform Your Idea Into Reality with Finsweet</h1>
+            <p className="text-[16px] w-[55%]" >The entire Finsweet team knows what's good with Webflow and you can too with 1 week and a good attitude.</p>
+            <button className="bg-red-500 py-2 px-3  text-[16px] my-4 hover:bg-red-700">Request Quote</button>
+          </div>
+          <div className="right_hero_section ">
+           <img src={hero_image} className="w-full" alt="her_image" />
+          </div>
+
+        </div>
+      </header>
     </div>
     </div>
   );
