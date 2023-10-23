255861ea3fd20beb134a3f3990e1efddd4961c36
kailash <kailashrajut0575@gmail.com>
2023-10-23 20:19:39 +0530

    navbar_done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-
-import './App.css'
-
+import "./App.css";
+import Logo from "../src/assets/images/navbar_image/Logo.svg";
 function App() {
-
   return (
-    <>
-       <h1 className='text-lg text-center' >vite ,typescript and tailwand css</h1>
-    </>
-  )
+    <div className="main_container_navbar bg-slate-950">
+
+    <div className="container mx-auto py-4">
+      <nav className="flex justify-between">
+        <div>
+          {" "}
+          <img src={Logo} alt="navbar_logo" />
+        </div>
+        <div className="flex text-lg text-white  list-none gap-5">
+          <li>Home</li>
+          <li>Services</li>
+          <li> Company</li>
+          <li>Careen</li>
+          <li>Blog</li>
+          <li>Contact US</li>
+          <li>Clone project</li>
+
+        </div>
+      </nav>
+    </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
