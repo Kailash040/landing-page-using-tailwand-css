2823c98f8cf631dddd7c4d26b1b3c63bed3e9e49
kailash <kailashrajut0575@gmail.com>
2024-01-25 19:01:00 +0530

    footer-ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+
 import './App.css'
 import Home from './component/Home'
 import Hero from './component/Hero'
+import Footer from './component/Footer'
 
 const App = () => {
   return (
     <div>
       <Home/>
       <Hero/>
+      <Footer/>
     </div>
   )
 }
